d616248ec1f259d629d01e195cedb4e50dcc9187
Peter PayteR Gašparík <payter86@gmail.com>
2019-04-01 00:50:58 +0200

    refactoring

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
-  .sass('scss/cypher-bulma-utilities.scss', 'public/dist')
+  .sass('scss/_all.scss', 'public/dist')
   .browserSync({
     proxy: 'http://127.0.0.1:19021',
     files: ["public/**/*"],
