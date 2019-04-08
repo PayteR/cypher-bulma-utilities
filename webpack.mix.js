9a6c2e37ca1e3109519ec7c4163fa5daff4e6f31
Peter PayteR Gašparík <payter86@gmail.com>
2019-04-08 05:54:39 +0200

    toopltip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,14 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
-  .sass('scss/_all.scss', 'public/dist')
-  .browserSync({
-    proxy: 'http://127.0.0.1:19021',
-    files: ["public/**/*"],
-    port: 19023,
-    ui: {
-      port: 19024
-    },
-    watch: true
-  });
+// disable for now
+// mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
+//   .sass('scss/_all.scss', 'public/dist')
+//   .browserSync({
+//     proxy: 'http://127.0.0.1:19021',
+//     files: ["public/**/*"],
+//     port: 19023,
+//     ui: {
+//       port: 19024
+//     },
+//     watch: true
+//   });
